2705679c0bda445ae6f41dd13d58ed1dd571808d
Diou Medeiros <diou.182@gmail.com>
2020-07-06 23:49:19 -0300

    feat: Created Toast hooks context

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
-import AuthContext from './context/AuthContext';
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
   <>
-    <AuthContext.Provider value={{ name: 'Diou' }}>
+    <AppProvider>
       <SignIn />
-    </AuthContext.Provider>
+    </AppProvider>
+
     <GlobalStyle />
   </>
 );
